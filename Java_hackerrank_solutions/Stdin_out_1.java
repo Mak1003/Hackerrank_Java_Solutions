cc6558f7b415cf9a68ffe03b6ce7bbe7cb9fac4b
MAK <40379551+Mak1003@users.noreply.github.com>
2025-07-28 22:35:31 +0530

    commit second

---
+++ Java_hackerrank_solutions/Stdin_out_1.java
@@ -0,0 +1,20 @@
+package Java_hackerrank_solutions; // dont copy this line
+
+import java.util.Scanner;
+
+public class Stdin_out_1 { // change the class name with the already given name
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+    }
+
+}
