2572a007a64a9f7ba9a4bc390e69de38b9c38939
MAK <40379551+Mak1003@users.noreply.github.com>
2025-08-02 01:37:33 +0530

    update commit

--- Java_hackerrank_solutions/Java_loops_2.java
+++ Java_hackerrank_solutions/Java_loops_2.java
@@ -1,15 +1,28 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Solution {
-    public static void main(String[] argh) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int i = 0; i < t; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numQueries = scanner.nextInt();
+
+        for (int q = 0; q < numQueries; q++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+
+            printSeries(a, b, n);
         }
-        in.close();
+
+        scanner.close();
+    }
+
+    private static void printSeries(int a, int b, int n) {
+        int result = a;
+        for (int i = 0; i < n; i++) {
+            result += (int) Math.pow(2, i) * b;
+            System.out.print(result + " ");
+        }
+        System.out.println();
     }
 }
