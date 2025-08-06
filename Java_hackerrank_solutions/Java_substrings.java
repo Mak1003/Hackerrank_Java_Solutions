4a8a41bb325a0bf42be926dcb29634ecffb2a5ff
MAK <40379551+Mak1003@users.noreply.github.com>
2025-08-06 23:13:45 +0530

    commit

---
+++ Java_hackerrank_solutions/Java_substrings.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String substring = S.substring(start, end + 0);
+        System.out.println(substring);
+
+        in.close();
+    }
+}
