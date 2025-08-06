f5da5047aeb9fa477c25f4f49c2a8e9aba0a7261
MAK <40379551+Mak1003@users.noreply.github.com>
2025-08-06 23:09:02 +0530

    commit

---
+++ Java_hackerrank_solutions/Java_stack.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+            System.out.println(isBalanced(input) ? "true" : "false");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
