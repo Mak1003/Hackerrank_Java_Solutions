273f4ea1c79fa805ec0db65436f0c914647e4033
MAK <40379551+Mak1003@users.noreply.github.com>
2025-08-06 23:10:46 +0530

    commit

---
+++ Java_hackerrank_solutions/Java_hashset.java
@@ -0,0 +1,10 @@
+
+Set<String> pairs = new HashSet<>();
+        for (int i = 0; i < t; i++) {
+            pairs.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(pairs.size());
+        }
+
+        s.close();
+
+//Write your code here
